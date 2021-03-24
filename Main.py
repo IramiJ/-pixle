26180259f937a22cd06ab1bad635c1b13f2ee660
Jason <yugiohduel3@gmail.com>
2021-03-24 16:00:20 +0100

    added my tile_loader. Still gotta do some stuff though

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import pygame,sys,math,os,random
 import data.scripts.block_handler as b_handler
 import data.scripts.core_funcs as core_funcs
-import data.scripts.tile_bar as tile_bar
 import data.scripts.text as Font
 from pygame.locals import *
 #------initialization------------------------------------------------------------------------------------
@@ -12,7 +11,7 @@ screen = pygame.display.set_mode(SCREEN_SIZE,0,32)
 surface = pygame.Surface((SCREEN_SIZE[0] // 2, SCREEN_SIZE[1] // 2))
 pygame.display.set_caption('!Pixle')
 #-----block handler and Stuff----------------------------------------------------------------------------
-Tile_size = 24
+Tile_size = 16
 #------font rendering------------------------------------------------------------------------------------
 font = Font.Font('data/images/fonts/small_font.png', (255,255,255))
 #------spritesheet handler-------------------------------------------------------------------------------
@@ -22,6 +21,20 @@ def load_spritesheet(path):
         tiles.append(pygame.image.load(path+'/'+img))
     return tiles
 tiles = load_spritesheet('data/images/tilesets/Gras_tiles')
+#------tile bar-----------------------------------------------------------------------------------------
+class tile_bar():
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.width = surface.get_width() // 6
+        self.height = surface.get_height()
+    def render(self):
+        pass
+    def redner_tiles(self):
+        pass
+    def render_tile_names(self):
+        pass
+
 if __name__ == '__main__':
     while True:
         font.render('!Pixle', surface, [0, 0])
